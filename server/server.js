e2d756c6afbc6bb42a0043ab2acd6e49b2a2349e
charukaonline <nuwanthakonline@gmail.com>
2025-04-09 11:28:20 +0530

    #

--- server/server.js
+++ server/server.js
@@ -33,4 +33,4 @@ app.use("/api/dcpower", dcPowerRoutes);
 app.use("/api/userAuth", userAuthRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT} Hello world`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
