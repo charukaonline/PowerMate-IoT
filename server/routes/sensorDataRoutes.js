c295b76a7aad1809c4d71786d5d7976daa2247b7
charukaonline <nuwanthakonline@gmail.com>
2025-04-16 15:28:45 +0530

    #

--- server/routes/sensorDataRoutes.js
+++ server/routes/sensorDataRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { storeSensorData } = require("../controllers/sensorDataController");
+const { storeSensorData, storeTempData } = require("../controllers/sensorDataController");
 
 const router = express.Router();
 
 // POST endpoint to store all sensor data - no authentication required
 router.post("/data", storeSensorData);
+router.post("/temp-data", storeTempData); // For temperature data
 
 module.exports = router;
