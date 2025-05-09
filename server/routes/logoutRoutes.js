a094083187a441d7a0605bd56064562d0200d21e
RajanithBandara <RajanithBandara123@gmail.com>
2025-05-09 12:09:41 +0530

    logout done

---
+++ server/routes/logoutRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { signup, login, logout, checkAuth, myData} = require('../controllers/userAuthController');
+const { verifyToken } = require('../middleware/verifyToken');
+
+const router = express.Router();
+
+router.get("/check-auth", verifyToken, checkAuth);
+
+router.get("/logout", logout);
+
+router.get("/me", myData);
+
+module.exports = router;
