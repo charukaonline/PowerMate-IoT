3494e9c5e5e5e66f63224f02c2b614c009e96365
DonSanoj <sanojaminda@gmail.com>
2025-05-09 18:31:50 +0530

    config shadcn UI

--- client-new/src/App.jsx
+++ client-new/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Button } from './components/ui/button'
 
 function App() {
   return (
-    <div><h1 className='text-center'>Hello world!</h1></div>
+    <div>
+
+      <h1 className='text-center'>Hello world!</h1>
+
+      <Button>Click me</Button>
+    </div>
   )
 }
 
